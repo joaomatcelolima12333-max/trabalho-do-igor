d5a1ff41ca2046f2846a5b98b9a9246499f04be4
joaomatcelolima12333-max <joaomatcelolima12333@gmail.com>
2026-06-12 11:10:05 -0300

    Update README with project details
    Added project details including team members, professor, and course information to README.

---
+++ Add.js
@@ -0,0 +1,15 @@
+# Trabalho de Programação Mobile
+
+## Integrantes
+- João Marcelo Lima Matos Alves Cavalcante - 01814302
+- Antonio Leandro de Moraes Mendes - 01815675
+- Pedro Ryan Bacelar Pereira - 01808041
+
+## Professor
+Igor Revoredo
+
+## Curso
+Ciência da Computação - UNINASSAU
+
+## Descrição
+Aplicativo de gerenciamento de tarefas desenvolvido em React Native.
